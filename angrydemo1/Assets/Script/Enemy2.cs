e9c50def488827cb8286226d3a94874c304407cb
unknown <trankimsen1681998@gmail.com>
2018-09-05 01:49:52 +0700

    fix all

---
+++ angrydemo1/Assets/Script/Enemy2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour {
+    public float health = 4f;
+    void OnCollisionEnter2D (Collision2D enemybird)
+    {
+        Debug.Log(enemybird.relativeVelocity.magnitude);
+          if ( enemybird.relativeVelocity.magnitude>health)
+            {
+                Die();
+            }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
