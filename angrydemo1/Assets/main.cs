85bbc4e141c90f5c2544ac5f3edddc0c0375e431
unknown <trankimsen1681998@gmail.com>
2018-09-05 11:45:58 +0700

    abc

---
+++ angrydemo1/Assets/main.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void PlayGame()
+    {
+        Application.LoadLevel("choose_level");
+    }
+}
