ee62cae13afb91ee0542e992575eeab75fe93e82
unknown <trankimsen1681998@gmail.com>
2018-09-05 18:04:17 +0700

    add all level

---
+++ angrydemo1/Assets/level2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void PlayGame()
+    {
+        Application.LoadLevel("level2");
+    }
+}
